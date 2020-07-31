3e35b29ceffb443714587bb4fbfae7590a5d35f4
woochan456 <60054739+woochan456@users.noreply.github.com>
2020-07-31 16:20:23 +0900

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -29,7 +29,5 @@ async def on_message(message):
                 except:
                     pass
 
-
-
-access_token = os.environ["TOKEN_TOKEN"]
+access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
